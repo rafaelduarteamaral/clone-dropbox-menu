fe7d00ba2f6e35a6714aa199d989eebc9e4d1d35
Rafael Amaral <rafael.duarte.amaral@gmail.com>
2020-08-16 17:16:51 -0300

    first commit

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, Content, HeaderWrapper, Header, DropboxLogo } from './styles';
+
+interface Props {
+    variant: 'blue' | 'beige' | 'white' | 'black';
+    title: string;
+    description: string;
+}
+
+const Section: React.FC<Props> = ({ variant, title, description }) => {
+    const buttomVariant:Number = Math.round(Math.random());
+
+    return (
+        <Container className={variant}>
+
+            <HeaderWrapper>
+                <Header>
+                    <h1>
+                        <DropboxLogo />
+                        <span>Dropbox</span>
+                    </h1>
+                    <button>{ buttomVariant === 0 ? 'Acessar' : 'Interagir' }</button>
+                </Header>
+            </HeaderWrapper>
+
+            <Content>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </Content>
+        </Container>
+    );
+};
+
+export default Section;
